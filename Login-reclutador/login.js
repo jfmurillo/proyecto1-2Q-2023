722b5c63c75ecebf1b7e774b7c6f1da374035527
Valeria Mora <valeria.mora2799@gmail.com>
2023-07-14 15:39:00 -0600

    Opción de iniciar sesión como cualquier tipo de usuario desde la vista principal
    Signed-off-by: Valeria Mora <valeria.mora2799@gmail.com>

---
+++ Login-reclutador/login.js
@@ -0,0 +1,11 @@
+  document.getElementById('login-button').addEventListener('click', function(event) {
+  event.preventDefault();
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  
+  if(email === "" || password === "") {
+    alert("Por favor rellene todos los campos");
+  } else {
+    window.location.href = '../Vista Reclutador/Inicio/InicioReclutador.html';
+  }
+});
--- Login/login.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('login-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    window.location.href = '../Vista Administrador/Inicio/InicioAdministrador.html';
-  });
