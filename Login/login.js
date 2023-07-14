974fd94773542666cc992d41c01cef54252a9a2b
Valeria Mora <valeria.mora2799@gmail.com>
2023-07-14 14:49:39 -0600

    Iniciar sesión
    Signed-off-by: Valeria Mora <valeria.mora2799@gmail.com>

---
+++ Login/login.js
@@ -0,0 +1,4 @@
+document.getElementById('login-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = '../Vista Administrador/Inicio/InicioAdministrador.html';
+  });
